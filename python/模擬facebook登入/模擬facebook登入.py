73ae2f669a07a65a3bb444756b8dc7133f686282
Mei  Feng Liao <110496696+Heydede09@users.noreply.github.com>
2023-05-24 17:31:55 +0800

    Add files via upload

---
+++ python/模擬facebook登入/模擬facebook登入.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 06:34:03 2023
+
+@author: User
+"""
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+usr=input("請輸入帳號(email或手機):") #usr="0920603248"
+pwd=input("請輸入密碼：")# pwd="nt49546132"
+driver=webdriver.Chrome()
+driver.get("https://www.facebook.com/")
+print('opend facebook')
+sleep(2)
+
+username_box=driver.find_element('id','email')
+username_box.send_keys(usr)
+print("email id entered")
+sleep(2)
+
+password_box=driver.find_element('id','pass')
+password_box.send_keys(pwd)
+print("pass id entered")
+sleep(2)
+
+login_box=driver.find_element(By.CSS_SELECTOR,"button[name='login']")
+login_box.click()
+print("ok")
